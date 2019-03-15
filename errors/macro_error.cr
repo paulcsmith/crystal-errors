c72aef997dcaae9d5ae204329533bed5cc0a4ee2
Paul Smith <paulcsmith0218@gmail.com>
2019-03-14 22:18:16 -0400

    Improve macro

--- errors/macro_error.cr
+++ errors/macro_error.cr
@@ -4,10 +4,14 @@ puts <<-ERROR
 #{"Error: undefined method 'not_a_method' for String".colorize.yellow.bold}
 #{"Showing last frame. Use --verbose for full trace.".colorize.dim}
 
+Expanded macro from src/my_cool_macro.cr:
+
+ #{"> 14 |".colorize.dim} "this is ok"
+ #{"> 15 |".colorize.green} #{%("this is not ok".not_a_method).colorize.bold}
+
 In src/my_app.cr:
 
-#{"> 14 |".colorize.dim} "this is ok"
-#{"> 15 |".colorize.green} #{%("this is not ok".not_a_method).colorize.bold}
+ #{"> 16 |".colorize.green} #{"my_cool_macro".colorize.bold}
 
 
 ERROR
