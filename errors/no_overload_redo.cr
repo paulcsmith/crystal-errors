74b3d270180ed220cf820ec9cd89258236c2062a
Paul Smith <paulcsmith0218@gmail.com>
2019-06-17 11:25:59 -0400

    Redo no overloads

--- errors/no_overload_redo.cr
+++ errors/no_overload_redo.cr
@@ -3,12 +3,12 @@ require "colorize"
 puts <<-ERROR
 #{"Showing last frame. Use --verbose for full trace.".colorize.dim}
 
-#{"Error: no overload matches 'something' with type Int32.".colorize.red.bold}
+In #{"src/my_app.cr".colorize.underline}
 
-In #{"src/my_app.cr".underline}
-
- #{"14 |".colorize.dim} #{"full_name(1)"}
+ #{"14 |".colorize.dim} #{"full_name(1)".colorize.bold}
       #{"^~~~~~~~~".colorize.green}
+
+#{"Error:".colorize.red.bold} no overload matches 'full_name' with type Int32.
 
 Overloads are:
  - full_name(name : String)
