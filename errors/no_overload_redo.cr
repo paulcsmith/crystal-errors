03dc38c531680da956140ce7a74dc35b9b06e454
Paul Smith <paulcsmith0218@gmail.com>
2019-11-01 10:51:09 -0400

    Errors with type sig

--- errors/no_overload_redo.cr
+++ errors/no_overload_redo.cr
@@ -10,12 +10,16 @@ puts <<-ERROR
 #{"Showing concise trace:".colorize.bold} #{"(Use --error-trace to expand)".colorize.dim}
 
  #{arrow} #{"handler.payload.new(context, handler.params).perform_action".colorize.green.bold}
+   #{"Lucky::Action#perform_action".colorize.bold}
    #{"lib/lucky/src/lucky/route_handler.cr:10".colorize}
  #{arrow} #{"response = call".colorize.green.bold}
+   #{"Lucky::Action#response".colorize.bold}
    #{"lib/lucky/src/lucky/renderable.cr:97".colorize}
  #{arrow} #{"html Users::ShowPage, user: user".colorize.green.bold}
+   #{"Users::Show#html(Users::ShowPage, User)".colorize.bold}
    #{"src/actions/users/show.cr.cr:11".colorize}
  #{arrow} #{"render_user(@user)".colorize.green.bold}
+   #{"Users::ShowPage#render_user(User)".colorize.bold}
    #{"src/actions/users/show_page.cr.cr:11".colorize}
 
 #{"Problem found in".colorize.bold} #{"src/pages/users/show_page.cr:23".colorize.underline.bold}
