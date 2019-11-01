a96ec81777727798f4edfcae8a3c5057d38a3ad1
Paul Smith <paulcsmith0218@gmail.com>
2019-11-01 10:48:25 -0400

    Nicer colors. Green and yellow

--- errors/no_overload_redo.cr
+++ errors/no_overload_redo.cr
@@ -9,19 +9,19 @@ puts <<-ERROR
 
 #{"Showing concise trace:".colorize.bold} #{"(Use --error-trace to expand)".colorize.dim}
 
- #{arrow} #{"handler.payload.new(context, handler.params).perform_action"}
-   #{"lib/lucky/src/lucky/route_handler.cr:10".colorize.dim}
- #{arrow} #{"response = call"}
-   #{"lib/lucky/src/lucky/renderable.cr:97".colorize.dim}
- #{arrow} #{"html Users::ShowPage, user: user"}
-   #{"src/actions/users/show.cr.cr:11".colorize.dim}
- #{arrow} #{"render_user(@user)"}
-   #{"src/actions/users/show_page.cr.cr:11".colorize.dim}
+ #{arrow} #{"handler.payload.new(context, handler.params).perform_action".colorize.green.bold}
+   #{"lib/lucky/src/lucky/route_handler.cr:10".colorize}
+ #{arrow} #{"response = call".colorize.green.bold}
+   #{"lib/lucky/src/lucky/renderable.cr:97".colorize}
+ #{arrow} #{"html Users::ShowPage, user: user".colorize.green.bold}
+   #{"src/actions/users/show.cr.cr:11".colorize}
+ #{arrow} #{"render_user(@user)".colorize.green.bold}
+   #{"src/actions/users/show_page.cr.cr:11".colorize}
 
 #{"Problem found in".colorize.bold} #{"src/pages/users/show_page.cr:23".colorize.underline.bold}
 
- #{"23 |".colorize.dim} #{"full_name(@user.age)".colorize}
-      #{"^--------".colorize.green}
+ #{"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
+                #{"^--------".colorize.yellow.bold}
 
 #{"Error: no overload matches 'full_name' with type Int32.".colorize.yellow.bold}
 
