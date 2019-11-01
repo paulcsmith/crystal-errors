0c6d7fc1ff2b2ff3ecb3ea824c4becc06404a484
Paul Smith <paulcsmith0218@gmail.com>
2019-10-31 22:21:10 -0400

    Try showing type signature

--- errors/no_overload_redo.cr
+++ errors/no_overload_redo.cr
@@ -3,17 +3,19 @@ require "colorize"
 puts <<-ERROR
 #{"Showing concise trace. Use --error-trace to expand.".colorize.dim}
 
-- #{"html Users::ShowPage, user: user".colorize.bold}
+- #{"html Users::ShowPage, user: user".colorize.bold.yellow}
+  #{"Users::Show#html(Users::ShowPage, User)".colorize.bold}
   src/actions/users/show.cr.cr:11
-- #{"render_user(@user)".colorize.bold}
+- #{"render_user(@user)".colorize.bold.yellow}
+  #{"Users::ShowPage#render_user(User)".colorize.bold}
   src/actions/users/show_page.cr.cr:11
 
 In #{"src/pages/users/show_page.cr:23".colorize.underline}
 
  #{"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
-      #{"^~~~~~~~~".colorize.green}
+      #{"^--------".colorize.green}
 
-#{"Error:".colorize.red.bold} no overload matches 'full_name' with type Int32.
+#{"Error: no overload matches 'full_name' with type Int32.".colorize.yellow.bold}
 
 Overloads are:
  - full_name(name : String)
