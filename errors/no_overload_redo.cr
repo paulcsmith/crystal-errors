01750b40fc6be70de2878dd65bb20f25611ed35c
Paul Smith <paulcsmith0218@gmail.com>
2019-11-14 18:14:35 -0500

    Line numbers and stuff

--- errors/no_overload_redo.cr
+++ errors/no_overload_redo.cr
@@ -10,27 +10,29 @@ puts <<-ERROR
 
 #{"Beginning of trace:".colorize.bold} #{"(Use --error-trace to expand)".colorize.dim}
 
-#{"In Lucky::Action#perform_action".colorize.bold.cyan}
-#{"handler.payload.new(context, handler.params).perform_action".colorize.bold}
-#{"lib/lucky/src/lucky/route_handler.cr:10".colorize}
+#{"lib/lucky/src/lucky/route_handler.cr:10".colorize.cyan.bold}
+#{"In Lucky::Action#perform_action".colorize.bold}
+#{"10 |".colorize.dim} #{"handler.payload.new(context, handler.params).perform_action".colorize}
 
-#{"In Lucky::Action#response".colorize.bold.cyan}
-#{"response = call".colorize.bold}
-#{"lib/lucky/src/lucky/renderable.cr:97".colorize}
+#{"lib/lucky/src/lucky/renderable.cr:97".colorize.cyan.bold}
+#{"In Lucky::Action#response".colorize.bold}
+#{"97 |".colorize.dim} #{"response = call".colorize}
 
-#{"In Users::Show#call(Users::ShowPage, User)".colorize.bold.cyan}
-#{"html Users::ShowPage, user: user".colorize.bold}
-#{"src/actions/users/show.cr.cr:11".colorize}
+#{"src/actions/users/show.cr.cr:11".colorize.bold.cyan}
+#{"In Users::Show#call(Users::ShowPage, User)".colorize.bold}
+#{"11 |".colorize.dim} #{"html Users::ShowPage, user: user".colorize}
 
-#{"In Users::ShowPage#render_user(User)".colorize.bold.cyan}
-#{"render_user(@user)".colorize.bold}
-#{"src/actions/users/show_page.cr.cr:11".colorize}
+#{"src/actions/users/show_page.cr.cr:11".colorize.bold.cyan}
+#{"In Users::ShowPage#render_user(User)".colorize.bold}
+#{"11 |".colorize.dim} #{"render_user(@user)".colorize}
 
-#{"In".colorize.bold.yellow} #{"Users::ShowPage#render_user(User)".colorize.bold.yellow}
-#{"src/pages/users/show_page.cr:23".colorize}
+#{"---".colorize.dim}
 
- #{"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
-                #{"^--------".colorize.yellow.bold}
+#{"src/pages/users/show_page.cr:23".colorize.bold.yellow}
+#{"In".colorize.bold} #{"Users::ShowPage#render_user(User)".colorize.bold}
+
+#{"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
+               #{"^--------".colorize.yellow.bold}
 
 #{"Error:".colorize.yellow.bold} #{"no overload matches 'full_name' with type Int32.".colorize.bold}
 
@@ -43,5 +45,5 @@ ERROR
 # {"Error: no overload matches 'full_name' with type Int32.".colorize.yellow.bold}
 # {"Error:".colorize.bold.yellow} no overload matches 'full_name' with type Int32.
 
-  #{"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
-  #{"src/pages/users/show_page.cr:23".colorize.underline}
+# {"23 |".colorize.dim} #{"full_name(@user.age)".colorize.bold}
+# {"src/pages/users/show_page.cr:23".colorize.underline}
